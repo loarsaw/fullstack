109debb5808d0efa69f8e3a45cba9a9c7a0186a4
loarsaw <amanahmed1385@gmail.com>
2023-11-12 18:17:17 +0530

    completed server

---
+++ server/controllers/userController.js
@@ -0,0 +1,13 @@
+export const getUsers = async (req, res) => {
+    const response = await fetch(`https://api.github.com/users/github`);
+    const resData = await response.json()
+    res.status(200).json(resData)
+}
+
+export const getUser = async (req, res) => {
+    const response = await fetch(`https://api.github.com/users/${req.params.id}`);
+    const resData = await response.json();
+    console.log(resData)
+    res.status(200).json(resData)
+
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import userRoute from "./routes/users.js"
 const app = express()
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+app.use("/api/user", userRoute)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
-
-
